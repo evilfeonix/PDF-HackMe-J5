371edacd1d19afb30bc993603298b1c6f9d0384d
Evil feoniX <evilfeonix@gmail.com>
2025-01-22 07:42:47 +0100

    Update PAYLOAD.js

--- PAYLOAD.js
+++ PAYLOAD.js
@@ -1,6 +1,10 @@
-var authors = "EvilFeonix";
-var tools = "PDF-HackMe-J5";
-alert("Welcome To "+tools+", Created by "+authors+"!");
-alert("this is a python based-embedding script that allows you to simply craft your own malicious JavaScript payload to a PDF file");
-console.log(authors);
-console.log(tools);
+PAYLOAD()
+
+function PAYLOAD(){
+  var authors = "EvilFeonix";
+  var tools = "PDF-HackMe-J5";
+  alert("Welcome To "+tools+", Created by "+authors+"!");
+  alert("this is a python based-embedding script that allows you to simply craft your own malicious JavaScript payload to a PDF file");
+  console.log(authors);
+  console.log(tools);
+}
